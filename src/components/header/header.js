e3bc48933fd5f5e2ddde86494742f9e06cdf124c
gashy_jet <gashy_jet@yahoo.com>
2022-04-08 14:39:56 +0300

    Add loading status in header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,29 +4,38 @@ import { saveNewTodo } from "../todos/todosSlice";
 
 const Header = () => {
   const [text, setText] = useState("");
+  const [status, setStatus] = useState("idle");
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setText(e.target.value);
   };
 
-  const handleKeyDown = (e) => {
-    const textTrim = e.target.value.trim();
+  const handleKeyDown = async (e) => {
+    const textTrim = text.trim();
     if (e.key === "Enter" && textTrim) {
-      dispatch(saveNewTodo(textTrim));
+      setStatus("loading");
+      await dispatch(saveNewTodo(textTrim));
       setText("");
+      setStatus("idle");
     }
   };
+
+  let isLoading = status === "loading";
+  let placeholder = isLoading ? "" : "What needs to be done?";
+  let loader = isLoading ? <div className="loader" /> : null;
 
   return (
     <header className="header">
       <input
         className="new-todo"
-        placeholder="What needs to be done?"
+        placeholder={placeholder}
         value={text}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
+        disabled={isLoading}
       />
+      {loader}
     </header>
   );
 };
