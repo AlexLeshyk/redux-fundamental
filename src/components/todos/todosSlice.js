6e7da0a9ee2c7d7e58a6e2386a41c37b3fe6b74b
gashy_jet <gashy_jet@yahoo.com>
2022-04-08 13:48:28 +0300

    add loading status change initial state for

--- src/components/todos/todosSlice.js
+++ src/components/todos/todosSlice.js
@@ -2,13 +2,16 @@ import { createSelector } from "reselect";
 import { StatusFilters } from "../filters/filtersSlice";
 import { client } from "../../api/client";
 
-const initialState = [];
+const initialState = {
+  status: "idle",
+  todos: [],
+};
 
 // function nextTodoId(todos) {
 //   const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
 //   return maxId + 1;
 // }
-export const selectTodos = (state) => state.todos;
+export const selectTodos = (state) => state.todos.todos;
 
 export const selectFilteredTodos = createSelector(
   // First input selector: all todos
@@ -50,44 +53,70 @@ export const selectFilteredTodoIds = createSelector(selectFilteredTodos, (filter
 const todosReducer = (state = initialState, action) => {
   switch (action.type) {
     case "todos/todoAdded": {
-      return [...state, action.payload];
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
     }
     case "todos/todoToggled": {
-      return state.map((todo) => {
-        if (todo.id !== action.payload) {
-          return todo;
-        }
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      });
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id !== action.payload) {
+            return todo;
+          }
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }),
+      };
     }
     case "todos/colorSelected": {
       const { todoId, color } = action.payload;
-      return state.map((todo) => {
-        if (todo.id !== todoId) {
-          return todo;
-        }
-        return {
-          ...todo,
-          color,
-        };
-      });
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id !== todoId) {
+            return todo;
+          }
+          return {
+            ...todo,
+            color,
+          };
+        }),
+      };
     }
     case "todos/todoDeleted": {
-      return state.filter((todo) => todo.id !== action.payload);
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
     }
     case "todos/allCompleted": {
-      return state.map((todo) => {
-        return { ...todo, completed: true };
-      });
+      return {
+        ...state,
+        todos: state.todos.map((todo) => ({ ...todo, completed: true })),
+      };
     }
     case "todos/completedCleared": {
-      return state.filter((todo) => !todo.completed);
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => !todo.completed),
+      };
     }
     case "todos/todosLoaded": {
-      return action.payload;
+      return {
+        ...state,
+        status: "idle",
+        todos: action.payload,
+      };
+    }
+    case "todos/todosLoading": {
+      return {
+        ...state,
+        status: "loading",
+      };
     }
     default:
       return state;
@@ -121,8 +150,11 @@ export const todoAdded = (todo) => {
   };
 };
 
+export const todosLoading = () => ({ type: "todos/todosLoading" });
+
 export function fetchTodos() {
   return async (dispatch, getState) => {
+    dispatch(todosLoading());
     const response = await client.get("/fakeApi/todos");
     dispatch(todosLoaded(response.todos));
   };
